833d71a11169fe7169176962f4fd0830b9765d64
smirza <sheenm1530@gmail.com>
2020-02-27 15:27:53 -0800

    First commit

---
+++ src/VerfiyBsciCode.java
@@ -0,0 +1,13 @@
+
+public class VerfiyBsciCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =0;
+		for(i=0;i<10;i++)
+		{
+			System.out.println("Printed value for int i is: " +i);
+		}
+		System.out.println(" ");
+	}
+}
