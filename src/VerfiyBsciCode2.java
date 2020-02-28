2cbde535bc5ec6d3cb9233527c2d5e07863721cd
scuser15 <chawdshah@gmail.com>
2020-02-27 19:26:24 -0800

    Updated changes for condition i<12

--- src/VerfiyBsciCode2.java
+++ src/VerfiyBsciCode2.java
@@ -4,7 +4,7 @@ public class VerfiyBsciCode2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i =0;
-		for(i=0;i<10;i++)
+		for(i=0;i<12;i++)
 		{
 			System.out.println("Printed value for int i is: " +i);
 		}
