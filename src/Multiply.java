9af660b846554bde898d6d880027c3f52972e46e
Deepak <Deepak@LAPTOP-01K0RIDR>
2020-03-03 13:35:35 -0800

    New code added

---
+++ src/Multiply.java
@@ -0,0 +1,12 @@
+
+public class Multiply {
+	public static double multiply(double a, double b) {
+		return a * b;
+	}
+	
+	public static void main(String args[])
+	{
+		double result = multiply(20.01,10.10);
+		System.out.println("The result is: " +result);
+	}
+}
